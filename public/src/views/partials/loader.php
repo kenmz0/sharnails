e024eee08248da35f0979746ca7a0659c86afffd
kenmz0 <94247331+kenmz0@users.noreply.github.com>
2026-05-30 01:46:55 -0500

    Update loader.php paths for assets

--- public/src/views/partials/loader.php
+++ public/src/views/partials/loader.php
@@ -1,8 +1,8 @@
 <dialog id="loader">
     <div class="container">
-        <img src="/public/assets/img/logo_plain.webp" alt="">
+        <img src="/assets/img/logo_plain.webp" alt="">
         <div class="loader"></div>
         <p class="direction"></p>
     </div>
 </dialog>
-<script src="/public/assets/scripts/loader.js"></script>
+<script src="/assets/scripts/loader.js"></script>
